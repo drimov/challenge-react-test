f36946c9be948a1f62fefa1f082140a39f5447e6
Dimitri Lhopital <dimitrilho.gmail.com>
2022-12-05 10:50:46 +0400

    refactor: 🎨Suppresion de renderCahe dans test-utils.js

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -1,18 +1,12 @@
 import * as React from 'react'
 import { render as renderReactTestingLib } from '@testing-library/react'
 import { ThemeProvider } from '../context/themeContext'
-import { CacheProvider } from '../context/cacheContext'
 
 const renderTheme = (ui) => {
 	const Wrapper = ({ children }) => <ThemeProvider>{children}</ThemeProvider>
 
 	return renderReactTestingLib(ui, { wrapper: Wrapper })
 }
-const renderCache = (ui) => {
-	const Wrapper = ({ children }) => <CacheProvider>{children}</CacheProvider>
-
-	return renderReactTestingLib(ui, { wrapper: Wrapper })
-}
 
 export * from '@testing-library/react'
-export { renderTheme, renderCache }
+export { renderTheme }
